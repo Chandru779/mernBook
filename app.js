a6231ce33185babc8da9f8dc48175b8f5316ce05
Chandru779 <chanducse2000@gmail.com>
2022-01-23 18:11:53 +0530

    connected database and installed config module

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express()
 
 app.get('/', (req,res)=>res.send('Hello world'));
 
-const PORT = Process.env.PORT || 8080;
+connectDB();
+
+const PORT =process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on Port ${PORT}`);
